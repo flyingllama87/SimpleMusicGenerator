d3632ce490c72b8eecdffd97591683ec7ff44907
Morgan Robertson <morgan@home-pc>
2021-06-12 11:40:12 +1000

    Update VoteTracker Default Schema

--- web/VoteTracker/schema.sql
+++ web/VoteTracker/schema.sql
@@ -18,18 +18,7 @@ INSERT INTO scores VALUES ("majesty", 2);
 INSERT INTO scores VALUES ("oops", 2);
 INSERT INTO scores VALUES ("flaxseed", 5);
 INSERT INTO scores VALUES ("reroute", 10);
-
-
-/*
-            listOfSeeds.push_back(std::tuple<std::string, int>{"covid", 0});
-            listOfSeeds.push_back(std::tuple<std::string, int>{"partake", 0});
-            listOfSeeds.push_back(std::tuple<std::string, int>{"removal", 0});
-            listOfSeeds.push_back(std::tuple<std::string, int>{"going", 0});
-            listOfSeeds.push_back(std::tuple<std::string, int>{"caboose", 0});
-            listOfSeeds.push_back(std::tuple<std::string, int>{"serbian", 0});
-            listOfSeeds.push_back(std::tuple<std::string, int>{"amuser", 0});
-            listOfSeeds.push_back(std::tuple<std::string, int>{"majesty", 0});
-            listOfSeeds.push_back(std::tuple<std::string, int>{"oops", 0});
-            listOfSeeds.push_back(std::tuple<std::string, int>{"flaxseed", 0});
-            listOfSeeds.push_back(std::tuple<std::string, int>{"reroute", 0});
-            */
+INSERT INTO scores VALUES ("moistbox", 1);
+INSERT INTO scores VALUES ("slip", 3);
+INSERT INTO scores VALUES ("gurgling", 3);
+INSERT INTO scores VALUES ("street", 3);
